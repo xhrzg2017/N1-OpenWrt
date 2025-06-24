3f2960bdd28264c0d17bae8e7cdddf52a60d9436
xhrzg2017 <34803905+xhrzg2017@users.noreply.github.com>
2025-06-24 16:29:04 +0900

    Update diy.sh

--- N1-LEDE/diy.sh
+++ N1-LEDE/diy.sh
@@ -11,17 +11,23 @@ function git_sparse_clone() {
 
 # Add packages
 #添加科学上网源
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
 git clone -b 18.06 --single-branch --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone -b 18.06 --single-branch --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
-git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
+#git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
 #git clone --depth=1 https://github.com/sirpdboy/NetSpeedTest package/NetSpeedTest
 
 git clone -b v5-lua --single-branch --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
-git clone -b lua --single-branch --depth 1 https://github.com/sbwml/luci-app-alist package/alist
-git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/lucky
+#git clone -b lua --single-branch --depth 1 https://github.com/sbwml/luci-app-alist package/alist
+#git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/lucky
+
+git clone --depth=1https://github.com/kenzok8/openwrt-packages package/adguardhome
+git clone --depth=1 https://github.com/kenzok8/openwrt-packages package/luci-app-adguardhome
+git clone --depth=1 https://github.com/kenzok8/openwrt-packages package/adguardhome
+git clone --depth=1 https://github.com/kiddin9/kwrt-packages package/luci-app-openclash
+
 #添加自定义的软件包源
 #git_sparse_clone main https://github.com/kiddin9/kwrt-packages ddns-go
 #git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-ddns-go
